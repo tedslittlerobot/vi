9adb6f8cc24f09831acfcf4a468b9c15c002bcf3
Stef Horner <stef@tedslittlerobot.com>
2015-01-28 18:26:50 +0000

    add ifusercan blade tag

--- src/Providers/BladeServiceProvider.php
+++ src/Providers/BladeServiceProvider.php
@@ -3,9 +3,7 @@
 use Illuminate\Routing\Router;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
-use Vi\Users\User;
-
-class RouteServiceProvider extends ServiceProvider {
+class BladeServiceProvider extends ServiceProvider {
 
 	/**
 	 * Define the routes for the application.
@@ -15,6 +13,52 @@ class RouteServiceProvider extends ServiceProvider {
 	public function map()
 	{
 		$this->loadRoutesFrom(app_path('Http/routes.php'));
+	}
+
+	/**
+	 * Boot the service provider
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$blade = $this->app['view']->getEngineResolver()->resolve('blade')->getCompiler();
+
+		$tags = [
+			'IfUserCan',
+		];
+
+		foreach( $tags as $tag )
+		{
+			$method = "compile{$tag}";
+			$blade->extend( $this->{$method}() );
+		}
+	}
+
+	/**
+	 * Compile the ifusercan blade tag
+	 *
+	 * @return void
+	 */
+	public function compileIfUserCan()
+	{
+		/**
+		 * @param string|array $permissions
+		 *
+		 * @ifusercan( $permissions )
+		 *   something here
+		 * @endif
+		 */
+		return function($view, $compiler)
+		{
+			// @ifusercan('permission')
+			$view = preg_replace(
+				$compiler->createMatcher('ifusercan'),
+				'$1<?php if(Auth::user() && Auth::user()->can($2)): ?>',
+				$view
+			);
+			return $view;
+		});
 	}
 
 }
