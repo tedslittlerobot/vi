43f5eda0acaf116321813b71c2e658989a990dc8
Stef Horner <stef@tedslittlerobot.com>
2015-03-11 11:02:48 +0000

    add registration controller

---
+++ src/Core/Framework/Http/Controllers/Auth/RegistrationController.php
@@ -0,0 +1,19 @@
+<?php namespace Vi\Core\Framework\Http\Controllers\Auth;
+
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Routing\Redirector;
+use Illuminate\Http\Request;
+
+use Vi\Core\Framework\Http\Controllers\Controller;
+
+/**
+ * @Middleware("guest")
+ */
+class RegistrationController extends Controller {
+
+	public function __construct( Guard $guard )
+	{
+		$this->guard = $guard;
+	}
+
+}
