e29396a93ed6237d8c0364b75a4db4d378a15351
Stef Horner <stef@tedslittlerobot.com>
2015-05-06 15:47:53 +0100

    add example og builder macro

--- src/Display/Seo/Meta/OpenGraphBuilder.php
+++ src/Display/Seo/Meta/OpenGraphBuilder.php
@@ -14,4 +14,26 @@ class OpenGraphBuilder {
 		return sprintf('<meta property="og:%s" content="%s" />', $name, e($value));
 	}
 
+	/**
+	 * Generate a set of open graph tags for an image
+	 *
+	 * @param  string $url
+	 * @param  string $width
+	 * @param  string $height
+	 * @param  string $type
+	 * @param  string $secure
+	 * @return string
+	 */
+	public function imageTag( $url, $width = null, $height = null, $type = null, $secure = null )
+	{
+		$output = $this->tag( 'og:image:url', $url );
+
+		$output .= $type   ? $this->tag( 'og:image:type', $type ) : '';
+		$output .= $width  ? $this->tag( 'og:image:width', $width ) : '';
+		$output .= $height ? $this->tag( 'og:image:height', $height ) : '';
+		$output .= $secure ? $this->tag( 'og:image:secure_url', $secure ) : '';
+
+		return $output;
+	}
+
 }
